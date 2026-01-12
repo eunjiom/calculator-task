2109e67f35998161a1b0b91a1e3ca2ef5912a10d
eunjiom <norgg021@gmail.com>
2026-01-12 11:29:31 +0900

    정수형 받기 및 정수형 변환

---
+++ src/step1/Main.java
@@ -0,0 +1,19 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자를 입력하세요: ");
+        int num = Integer.parseInt(sc.nextLine());
+        System.out.println("a: " + num);
+
+        System.out.print("두번째 숫자를 입력하세요: ");
+        int num2 = Integer.parseInt(sc.nextLine());
+        System.out.println("b: " + num2);
+
+    }
+}
