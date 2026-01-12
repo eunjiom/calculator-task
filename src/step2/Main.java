d1b693573569a58dea328513700feca8f4af29a7
eunjiom <norgg021@gmail.com>
2026-01-12 16:30:36 +0900

    step2: 계산과 반복종료

--- src/step2/Main.java
+++ src/step2/Main.java
@@ -10,18 +10,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int i = 1;
-        while (i < 2){
+        while (i < 2) {
             System.out.print("첫번째 숫자를 입력하세요: ");
             int a = sc.nextInt();
+
             System.out.print("두번째 숫자를 입력하세요: ");
             int b = sc.nextInt();
 
             System.out.print("사칙연산을 입력하세요: ");
             char op = sc.next().charAt(0);
 
+            // 결과 받기
+            int result = calculator.calculator(a, b, op);
+            System.out.println("결과 = " + +result);
 
+            // 종료안내
+            System.out.print("더 진행하시겠습니까? (exit 입력 시 종료): ");
+            String exit = sc.next();
 
-
+            if (exit.equals("exit"))
+                i = 2;
 
         }
 
