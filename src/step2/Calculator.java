7dd699de8a293b9b596d36c4c80f97e61a83b3de
eunjiom <norgg021@gmail.com>
2026-01-12 16:04:14 +0900

    클래스 추가, main에 값 입력받기

---
+++ src/step2/Calculator.java
@@ -0,0 +1,42 @@
+package step2;
+
+import java.util.ArrayList;
+
+public class Calculator {
+
+    ArrayList<Integer> names = new ArrayList<>();
+
+    public int calculator(int a, int b, char op) {
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+
+            case '-':
+                result = a - b;
+                break;
+
+            case '*':
+                result = a * b;
+                break;
+
+            case '/':
+                if (b == 0) {
+                    System.out.println("b에 0을 입력할 수 없습니다");
+                    return  0;
+                }
+                result = a / b;
+                break;
+
+            default:
+                System.out.println("잘못된 연산자 입니다.");
+                return 0;
+
+        }
+
+        return result;
+    }
+
+}
---
+++ src/step2/Main.java
@@ -0,0 +1,34 @@
+package step2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        while (i < 2){
+            System.out.print("첫번째 숫자를 입력하세요: ");
+            int a = sc.nextInt();
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int b = sc.nextInt();
+
+            System.out.print("사칙연산을 입력하세요: ");
+            char op = sc.next().charAt(0);
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
