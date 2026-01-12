f75b4cdb420129a7d8b910c82f6296ef3d4efd82
eunjiom <norgg021@gmail.com>
2026-01-12 12:52:10 +0900

    while 반복문을 통해 계산 반복하기

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -7,53 +7,54 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫번째 숫자를 입력하세요: ");
-        int num = Integer.parseInt(sc.nextLine());
-        System.out.println("a: " + num);
+        int i = 1;
+        while (i < 2) {
+            System.out.print("첫번째 숫자를 입력하세요: ");
+            int num = Integer.parseInt(sc.nextLine());
+            System.out.println("a: " + num);
 
-        System.out.print("두번째 숫자를 입력하세요: ");
-        int num2 = Integer.parseInt(sc.nextLine());
-        System.out.println("b: " + num2);
+            System.out.print("두번째 숫자를 입력하세요: ");
+            int num2 = Integer.parseInt(sc.nextLine());
+            System.out.println("b: " + num2);
 
-        System.out.print("사칙연산을 입력하세요(+, -, *, /): ");
-        char charbox = sc.nextLine().charAt(0);
+            System.out.print("사칙연산을 입력하세요(+, -, *, /): ");
+            char charbox = sc.nextLine().charAt(0);
 
-        int result = 0;
+            int result = 0;
 
-        int sum = num + num2;
-        int sub = num - num2;
-        int mul = num * num2;
+            int sum = num + num2;
+            int sub = num - num2;
+            int mul = num * num2;
 
-        switch (charbox){
-            case '+':
-                System.out.println("a+b= " + sum);
-                break;
+            switch (charbox) {
+                case '+':
+                    System.out.println("a+b= " + sum);
+                    break;
 
-            case '-':
-                System.out.println("a-b= " + sub);
-                break;
+                case '-':
+                    System.out.println("a-b= " + sub);
+                    break;
 
-            case '*':
-                System.out.println("a*b= " + mul);
-                break;
+                case '*':
+                    System.out.println("a*b= " + mul);
+                    break;
 
-            case '/':
-                if (num2 == 0)
-            {
-                System.out.println("b에 0을 입력할 수 없습니다.");
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("b에 0을 입력할 수 없습니다. 다시 입력해주세요.");
+                    } else {
+                        int div = num / num2;
+                        System.out.println("a/b= " + div);
+                    }
+                    break;
+
+                default:
+                    System.out.println("잘못 입력하였습니다. 다시 입력해주세요.");
             }
-                else {
-                    int div = num / num2;
-                System.out.println("a/b= " + div);
-            }
-                break;
 
-            default:
-                System.out.println("다시 입력해주세요");
         }
 
 
-
-
     }
+
 }
