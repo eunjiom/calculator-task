411882b2bdf130c7ef7f9b272f1230310f0c30f8
eunjiom <norgg021@gmail.com>
2026-01-19 10:33:56 +0900

    step1: 2회차 수정

--- src/step1/Main.java
+++ src/step1/Main.java
@@ -6,66 +6,68 @@ import java.util.Scanner;
 public class Main {
     static void main() {
 
+        // 스캐너 입력
         Scanner sc = new Scanner(System.in);
+        String exit = ""; // exit 입력 시 종료
 
-        int i = 1;
-        while (i < 2) {
+        // 반복문 사용
+        while (!exit.equals("exit")) { // exit가 아닐 때 반복
+            // 숫자값 입력받기
             System.out.print("첫번째 숫자를 입력하세요: ");
-            int num = Integer.parseInt(sc.nextLine());
-            System.out.println("a: " + num);
+            int num1 = Integer.parseInt(sc.nextLine());
+
+            // 0을 포함한 양의 정수값만 입력받기
+            if(num1 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다");
+                return;
+            }
 
             System.out.print("두번째 숫자를 입력하세요: ");
             int num2 = Integer.parseInt(sc.nextLine());
-            System.out.println("b: " + num2);
 
+            if(num2 < 0) {
+                System.out.println("0을 포함한 양의 정수만 입력 가능합니다");
+                return;
+            }
+
+            System.out.println("num1 :"+ num1 + "num2:" + num2);
+
+            // 사칙연산 기호 입력받기
             System.out.print("사칙연산을 입력하세요(+, -, *, /): ");
             char charbox = sc.nextLine().charAt(0);
 
             int result = 0;
 
-            int sum = num + num2;
-            int sub = num - num2;
-            int mul = num * num2;
-
             switch (charbox) {
-                case '+':
-                    System.out.println("a+b= " + sum);
-                    break;
-
-                case '-':
-                    System.out.println("a-b= " + sub);
-                    break;
-
-                case '*':
-                    System.out.println("a*b= " + mul);
-                    break;
-
-                case '/':
+                case '+' -> result = num1 + num2;
+                case '-' -> result = num1 - num2;
+                case '*' -> result = num1 * num2;
+                case '/' -> {
+                    // 연산 오류 (0으로 나눌 수 없음)
                     if (num2 == 0) {
-                        System.out.println("b에 0을 입력할 수 없습니다. 다시 입력해주세요.");
-                    } else {
-                        int div = num / num2;
-                        System.out.println("a/b= " + div);
+                        System.out.println("0으로 나눌 수 없습니다");
+                        return;
                     }
-                    break;
+                    result = num1 / num2;
+                }
 
-                default:
+                    // 입력한 값이 연산기호가 아닌 경우
+                default -> {
                     System.out.println("잘못 입력하였습니다. 다시 입력해주세요.");
-                    break;
+                    return;
+                    }
+            }
+
+            System.out.println("결과 ="+ result);
+            System.out.println("종료를 원하면 exit 입력");
+            exit = sc.nextLine();
 
             }
-            {
-            System.out.print("더 진행하시겠습니까? (exit 입력 시 종료): ");
-            String exit =  sc.nextLine();
 
-            if (exit.equals("exit"))
-                 i = 2;
-
-            }
 
         }
 
 
     }
 
-}
+
